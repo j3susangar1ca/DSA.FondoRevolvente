ad039fb722bc6b39d143e1c22a221e668f574b81
j3susangar1ca <j3susangar1ca@gmail.com>
2026-03-20 01:06:41 -0600

    3

---
+++ src/HCG.FondoRevolvente.Domain/Constants/LimitesNegocio.cs
@@ -0,0 +1,8 @@
+namespace HCG.FondoRevolvente.Domain.Constants;
+
+public static class LimitesNegocio
+{
+    public const decimal MontoMaximoFondoRevolvente = 75000m;
+    public const int MinimoCotizacionesRequeridas = 3;
+    public const int TiempoExpiracionBloqueoMinutos = 30;
+}
